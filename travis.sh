6c77a3afd6237bc67722fdff5881856399e2bf34
Jacob Carlborg <doob@me.com>
2015-05-03 14:45:35 +0200

    Build snippets on Travis since we support 64bit now.

--- travis.sh
+++ travis.sh
@@ -2,6 +2,13 @@
 
 set -e
 
+function install_dependencies {
+    sudo apt-get install -y libcairo2-dev libglib2.0-dev libpango1.0-dev \
+        libxfixes-dev libxdamage-dev libxcomposite-dev libxcursor-dev \
+        libxrandr-dev libxi-dev libxinerama-dev libxtst-dev libgtk2.0-dev \
+        libgnomeui-dev
+}
+
 function setup_super_project {
     cwd=`pwd`
     cd ..
@@ -11,13 +18,14 @@ function setup_super_project {
 }
 
 function fetch_submodules {
-    git submodule update --init base
+    git submodule update --init base org.eclipse.swt.snippets
 }
 
 function build {
-    rdmd build base swt
+    rdmd build base swt swtsnippets
 }
 
+install_dependencies
 setup_super_project
 fetch_submodules
 build
